19d498955e03dc35dafaa05dfc12e8b87844cb32
rodell <ryanodell2668@gmail.com>
2023-02-08 14:43:48 -0500

    Revert of server setup to showcase working site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import './App.css';
 import ShopCreate from './components/ShopCreate';
 import ShopList from './components/ShopList';
@@ -7,27 +7,12 @@ import ShopList from './components/ShopList';
 function App(){
   const [shop, setShop] = useState([]);
 
-  const fetchItems = async () => {
-    const response = await axios.get('http://localhost:3001/items');
-
-    //will now use reponse to update state data
-    setShop(response.data);
-  };
-
-  useEffect(() => { //called upon first site render
-    fetchItems();
-  }, []); //using empty array so useEffect is never called again
-
-  const editItemById = async (id, newName) => {
-    const response = await axios.put(`http://localhost:3001/items/${id}`, {
-      name: newName
-    });
-
+  const editItemById = (id, newName) => {
     //must find the shop object with like id and adjust it
     //use map for this, to find it, copy over properties and put new title in
     const itemObject = shop.map((item) => {
       if(item.id === id) {
-        return {...item, ...response.data};  //will end up returning truthy after copying over item properties from original object
+        return {...item, name: newName};  //will end up returning truthy after copying over item properties from original object
       }
 
       return item;  //ID we are not looking for gets returned back here without touching it
@@ -36,9 +21,7 @@ function App(){
     setShop(itemObject);
   };
 
-  const deleteItemById = async (id) => {
-    await axios.delete(`http://localhost:3001/items/${id}`);
-    
+  const deleteItemById = (id) => {
     //Use filter function to return a new copy of the array minus the id we don't object to equal to
     const updatedShop = shop.filter((item) => {
       return item.id !== id;
@@ -47,13 +30,11 @@ function App(){
     setShop(updatedShop);  //checks if state is different and will re-render if so
   };
 
-  const onCreateShop = async (item) => {
-    const response = await axios.post('http://localhost:3001/items', {
-      name: item
-    })
+  const onCreateShop = (item) => {
     const updatedShop = [
       ...shop,
-      response.data  //no longer manually adding data; but data returned to us 
+      {id: Math.round(Math.random() * 10000), //Not a guarantee there won't be duplicates (normally bad)
+      name: item} //rarely does React assign id; normally server side
     ];
     setShop(updatedShop);
   };
